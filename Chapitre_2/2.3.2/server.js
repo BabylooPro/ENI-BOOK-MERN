3f1b830b7158b0c7789dfc43b438e9b5e9eeb3ae
Max Remy <babyloopro@gmail.com>
2024-03-22 03:42:39 +0100

    added: chapitre 2.3.2

---
+++ Chapitre_2/2.3.2/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+	res.send("Hello World avec Express.js !");
+});
+
+app.listen(port, () => {
+	console.log(`Serveur Express écoutant sur le port ${port}`);
+});
