f8aeeda87a5c5be6934d19bfc4d3995e5ad78e9e
Max Remy <babyloopro@gmail.com>
2024-03-27 13:28:05 +0100

    added: chapitre 2.3.5

---
+++ Chapitre_2/2.3.5/index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const helmet = require("helmet");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+// Intégration des middlewares tiers
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan("tiny"));
+
+// Middleware de Logging personnalisé
+app.use((req, res, next) => {
+	console.log(`${req.method} ${req.url}`);
+	next();
+});
+
+// Middleware pour la journalisation des requêtes HTTP
+app.get("/", (req, res) => {
+	res.send("Page d'accueil");
+});
+
+// Route POST spécifique
+app.post("/exemple", (req, res) => {
+	res.send(req.body);
+});
+
+// Middleware spécifique à la route GET "/exemple"
+app.get(
+	"/exemple",
+	(req, res, next) => {
+		console.log("Middleware spécifique à la route /exemple");
+		next();
+	},
+	(req, res) => {
+		res.send("Réponse de la route /exemple");
+	}
+);
+
+// Route qui simule une erreur
+app.get("/erreur", (req, res, next) => {
+	const err = new Error("Erreur simulée"); // Créer une nouvelle erreur
+	// Vous pouvez également spécifier un code d'état HTTP personnalisé pour l'erreur
+	err.statusCode = 500; // Code d'état HTTP 500: Erreur interne du serveur
+
+	next(err); // Passez l'erreur à la fonction next pour la gestion des erreurs
+});
+
+// Middleware pour la Gestion des Erreurs
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).send("Quelque chose s'est mal passé !");
+});
+
+app.listen(3000, () => {
+	console.log("Serveur démarré sur le port 3000");
+});
